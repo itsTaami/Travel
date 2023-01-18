e0e2a1ff60e80fb862ed0ac9f3f1a036119d09ba
itsTaami <eegii0977@gmail.com>
2023-01-19 00:08:23 +0800

    Jan-19 Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Modal } from "@mui/material";
 import AuthPage from "./pages/Auth";
+import BasicModal from "./pages/Modal";
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <BasicModal/>
       <Router>
         <CssBaseline />
         <Routes>
